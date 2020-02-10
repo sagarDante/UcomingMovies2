833ac5f0205b686691769463a54fb24b1190ac1b
sagardante <meetcoolsagar@gmail.com>
2020-02-10 14:07:57 +0530

    Show Movies trailors

--- app/src/main/java/com/sagar/chincholkar/retrofit2/MainActivity.java
+++ app/src/main/java/com/sagar/chincholkar/retrofit2/MainActivity.java
@@ -1,48 +1,28 @@
 package com.sagar.chincholkar.retrofit2;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
 
 import com.sagar.chincholkar.retrofit2.Retrofit.APIInterface;
 import com.sagar.chincholkar.retrofit2.Retrofit.ApiClient;
-import com.sagar.chincholkar.retrofit2.Retrofit.GetUpcomingMovies.GetUpcomingMovies;
-import com.sagar.chincholkar.retrofit2.Retrofit.GetUpcomingMovies.Result;
-import com.sagar.chincholkar.retrofit2.Utils.PaginationScrollListener;
-import com.sagar.chincholkar.retrofit2.adapter.UpcomingMoviesApdapter;
 
-import java.util.ArrayList;
-import java.util.Collections;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
-    public final static String API_KEY = "b7cd3340a794e5a2f35e3abb820b497f";
     private static final String TAG = "Main Activity";
     private APIInterface apiInterface;
-    private RecyclerView recyclerView;
-    private SwipeRefreshLayout swipeRefreshLayout;
-    private static final int PAGE_START = 1;
-    private boolean isLoading = false;
-    private boolean isLastPage = false;
-    // limiting to 5 for this tutorial, since total pages in actual API is very large. Feel free to modify.
-    private int TOTAL_PAGES = 5;
-    private int currentPage = PAGE_START;
-    ArrayList<Integer> movieids;
 
+
+
+    private APIInterface.SortBy defaultApiSortBy = APIInterface.SortBy.RELEASE_DATE_DESCENDING;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,22 +30,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         apiInterface = ApiClient.getClient().create(APIInterface.class);
-        swipeRefreshLayout = findViewById(R.id.swipeRefresh);
-        recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        swipeRefreshLayout.setColorSchemeResources(R.color.orange, R.color.green, R.color.blue, R.color.red);
-        GetMoviesData();
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
+        Button btn_upcoming= findViewById(R.id.b_upcoming);
+        Button btn_Popular=findViewById(R.id.b_popular);
 
-                GetMoviesData();
-
-            }
-        });
-
-
+        btn_upcoming.setOnClickListener(this);
+        btn_Popular.setOnClickListener(this);
     }
 
     @Override
@@ -90,39 +59,16 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void GetMoviesData() {
-        /**
-         * GET List Resources
-         **/
-        Call<GetUpcomingMovies> call = apiInterface.getUpcomingMovies(API_KEY);
-        call.enqueue(new Callback<GetUpcomingMovies>() {
-            @Override
-            public void onResponse
-                    (Call<GetUpcomingMovies> call, Response<GetUpcomingMovies> response) {
-                ArrayList<Result> movies = new ArrayList<>();
-                movies.clear();
-                assert response.body() != null;
-                movies = response.body().getResults();
-                //Collections.reverse(movies);
 
-
-              /*  movieids=new ArrayList<>();
-                for(int i=0;i<movies.size();i++){
-                    movieids.add(movies.get(i).getId());
-
-                }
-                Log.d(TAG, "Number of movies received: " + movieids.size());*/
-                UpcomingMoviesApdapter upcomingMoviesApdapter = new UpcomingMoviesApdapter(MainActivity.this, movies);
-                recyclerView.setAdapter(upcomingMoviesApdapter);
-                swipeRefreshLayout.setRefreshing(false);
-            }
-
-            @Override
-            public void onFailure(Call<GetUpcomingMovies> call, Throwable t) {
-                // Log error here since request failed
-                Log.e(TAG, t.toString());
-            }
-        });
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.b_popular:
+                startActivity(new Intent(getApplicationContext(), PopularMoviesActivity.class));
+                break;
+            case R.id.b_upcoming:
+                startActivity(new Intent(getApplicationContext(),UpcomingMoviesActivity.class));
+                break;
+        }
     }
-
 }
--- app/src/main/java/com/sagar/chincholkar/retrofit2/adapter/UpcomingMoviesApdapter.java
+++ app/src/main/java/com/sagar/chincholkar/retrofit2/adapter/UpcomingMoviesApdapter.java
@@ -2,20 +2,38 @@ package com.sagar.chincholkar.retrofit2.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Lifecycle;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 import com.sagar.chincholkar.retrofit2.MovieDetailScreen;
+import com.sagar.chincholkar.retrofit2.MyCustomDialogFragment;
 import com.sagar.chincholkar.retrofit2.R;
+import com.sagar.chincholkar.retrofit2.Retrofit.APIInterface;
+import com.sagar.chincholkar.retrofit2.Retrofit.ApiClient;
+import com.sagar.chincholkar.retrofit2.Retrofit.GetMovieTrailorFromId.GetMovieTrailer;
 import com.sagar.chincholkar.retrofit2.Retrofit.GetUpcomingMovies.Result;
+import com.sagar.chincholkar.retrofit2.UpcomingMoviesActivity;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UpcomingMoviesApdapter extends RecyclerView.Adapter<UpcomingMoviesApdapter.ViewHolder> {
     private ArrayList<Result> listItems;
@@ -24,9 +42,28 @@ public class UpcomingMoviesApdapter extends RecyclerView.Adapter<UpcomingMoviesA
     private String imgid;
     private boolean Sadult;
     private String overview;
+    private static final int ITEM = 0;
+    private static final int LOADING = 1;
+    private boolean isLoadingAdded = false;
+    private APIInterface apiInterface;
+    private static final String TAG = "Adapter";
+    public UpcomingMoviesApdapter(Context c) {
+        this.c = c;
+    }
+    private Lifecycle lifecycle;
+    private YouTubePlayerView youTubePlayerView;
+    private FragmentManager mFragmentManager;
+
+    public UpcomingMoviesApdapter(Context c, ArrayList<Result> listItems,FragmentManager fragmentManager) {
+        this.c = c;
+        this.listItems = listItems;
+        this.mFragmentManager=fragmentManager;
+    }
+
     public UpcomingMoviesApdapter(Context c, ArrayList<Result> listItems) {
         this.c = c;
         this.listItems = listItems;
+
     }
 
     @Override
@@ -34,49 +71,56 @@ public class UpcomingMoviesApdapter extends RecyclerView.Adapter<UpcomingMoviesA
         View v= LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_view,parent,false);
 
+
+        apiInterface = ApiClient.getClient().create(APIInterface.class);
         return new UpcomingMoviesApdapter.ViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(final UpcomingMoviesApdapter.ViewHolder holder, int position) {
-        final Result Mlist=listItems.get(position);
+    public void onBindViewHolder(final UpcomingMoviesApdapter.ViewHolder holder, final int position) {
+        final Result Mlist = listItems.get(position);
 
-        Sadult=Mlist.getAdult();
-        imgid= String.valueOf(Mlist.getId());
+        Sadult = Mlist.getAdult();
+        imgid = String.valueOf(Mlist.getId());
         // overview=Mlist.getOverview();
-        imgurl=Mlist.getPosterPath();
+        imgurl = Mlist.getPosterPath();
         holder.title.setText(Mlist.getTitle());// DateUtils.formatDateTime(c, System.currentTimeMillis(), DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_NUMERIC_DATE | DateUtils.FORMAT_12HOUR)
         holder.releasedate.setText(Mlist.getReleaseDate());
-        if(!Sadult) {
+        if (!Sadult) {
             holder.adult.setText("U/A");
-        }else{
+        } else {
             holder.adult.setText("A");
         }
         // picasso
         Picasso.with(c)
-                .load("https://image.tmdb.org/t/p/w500"+imgurl)
+                .load("https://image.tmdb.org/t/p/w500" + imgurl)
                 .into(holder.posterimg);
 
-        holder.posterimg.setOnClickListener(new View.OnClickListener() {
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent i=new Intent(c, MovieDetailScreen.class);
+                Intent i = new Intent(c, MovieDetailScreen.class);
 
-                i.putExtra("id",Mlist.getId());
-                i.putExtra("title",Mlist.getTitle());
-                i.putExtra("Overview",Mlist.getOverview());
-                i.putExtra("vote",Mlist.getVoteAverage());
+                i.putExtra("id", Mlist.getId());
+                i.putExtra("title", Mlist.getTitle());
+                i.putExtra("Overview", Mlist.getOverview());
+                i.putExtra("vote", Mlist.getVoteAverage());
                 i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 c.startActivity(i);
 
             }
         });
 
+        holder.play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-
-
+               GetMoviesTrailors(Mlist.getId(), position);
+            }
+        });
     }
+
 
     @Override
     public int getItemCount() {
@@ -104,6 +148,51 @@ public class UpcomingMoviesApdapter extends RecyclerView.Adapter<UpcomingMoviesA
 
         }
 
+
+    }
+    private void GetMoviesTrailors(int id, final int pos) {
+        /* *
+         * GET List Resources
+         **/
+        final String[] key = new String[2];
+        Call<GetMovieTrailer> call = apiInterface.getUpcomingMoviesVideos(id, UpcomingMoviesActivity.API_KEY);
+        call.enqueue(new Callback<GetMovieTrailer>() {
+
+            @Override
+            public void onResponse
+                    (Call<GetMovieTrailer> call, Response<GetMovieTrailer> response) {
+                List<com.sagar.chincholkar.retrofit2.Retrofit.GetMovieTrailorFromId.Result> moviesTrailors = new ArrayList<>();
+                assert response.body() != null;
+                moviesTrailors = response.body().getResults();
+
+                key[0] =moviesTrailors.get(0).getKey();
+
+                Log.d(TAG, "onClick: " + key[0]);
+
+
+                FragmentTransaction ft = mFragmentManager.beginTransaction();
+                Fragment prev = mFragmentManager.findFragmentByTag("dialog");
+                if (prev != null) {
+                    ft.remove(prev);
+                }
+                ft.addToBackStack(null);
+                DialogFragment dialogFragment = new MyCustomDialogFragment();
+                Bundle args = new Bundle();
+                args.putString("videoid",key[0] );
+                dialogFragment.setArguments(args);
+                dialogFragment.show(ft, "dialog");
+               /* Intent i =new Intent(c, YoutubeVideoActivity.class);
+                i.putExtra("videoid",key[0]);
+                c.startActivity(i);*/
+                Log.d(TAG, "Number of movies received: " + moviesTrailors.size()+ key[0]);
+            }
+
+            @Override
+            public void onFailure(Call<GetMovieTrailer> call, Throwable t) {
+                // Log error here since request failed
+                Log.e(TAG, t.toString());
+            }
+        });
 
     }
 }
